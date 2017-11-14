6c76bef89e8caba2578a36d4552d94f3c7843f1e
ccceye <ccceye@gmail.com>
2017-11-14 10:45:15 +0800

    Add files via upload

---
+++ random-l.lisp
@@ -0,0 +1,22 @@
+
+(defvar *l-n* '("东山" "城东" "城西" "盘东" "梅云" "仙桥" "榕东" "渔湖" "试验区"))
+
+(defun rand-list (list-in)
+  "输入一个列表，随机排列后，再输出一个列表"
+  (let ((x list-in) (y (make-list (length list-in))) (z (length list-in)))
+    (dotimes (i z)
+      (let ((j (random (- z i))))
+	(setf (nth i y) (nth j x))
+	(setq x (remove-if #'(lambda (a) (equal a (nth j x))) x))))
+    y))
+
+(format t "~%抽签前的次序是：~%~{~16t~a~%~}" *l-n*)
+(format t "~%抽签后的顺序是：~%~{~16t~a~%~}" (rand-list *l-n*) )
+
+
+
+
+
+
+
+  
