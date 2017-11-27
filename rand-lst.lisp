e3209dc34c63d8c21db3d66727e69a9502ab273e
ccceye <ccceye@gmail.com>
2017-11-27 22:58:56 +0800

    Update rand-lst.lisp

--- rand-lst.lisp
+++ rand-lst.lisp
@@ -1,4 +1,3 @@
-
 (defun rand-lst (lst)
   "递归版本：输入一个列表，返回一个随机排列的列表"
   (if (null lst)
@@ -6,7 +5,6 @@
       (let ((r-l (nth (random (length lst)) lst)))
 	(cons r-l (rand-lst (remove r-l lst)))))) 
 
-
-2(defvar *l* '("aa" "bb" "cc" "dd" "ee"))
+(defvar *l* '("aa" "bb" "cc" "dd" "ee"))
 (format t "~% 原始列表：  ~a " *l*)
 (format t "~% 返回的列表：~a ~%" (rand-lst *l*)) 
